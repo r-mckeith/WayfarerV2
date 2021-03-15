ad437f868505eaf12fa93ff9754f58196b9d36dc
r-mckeith <r.mckeith@me.com>
2021-03-15 16:19:52 -0700

    add login functionality

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class City(models.Model):
@@ -11,6 +12,7 @@ class Post(models.Model):
   title = models.CharField(max_length=100)
   body = models.TextField(max_length=500)
   city = models.ForeignKey(City, on_delete=models.CASCADE)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
 
   def __str__(self):
     return f'{self.title}, {self.city}'
