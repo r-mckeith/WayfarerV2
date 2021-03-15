de243c97869659ecc16ae74b42d7fe3ba818a70b
r-mckeith <r.mckeith@me.com>
2021-03-15 16:07:34 -0700

    create post and city models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class City(models.Model):
+  name = models.CharField(max_length=100, unique=True)
+
+  def __str__(self):
+    return self.name
+
+class Post(models.Model):
+  title = models.CharField(max_length=100)
+  body = models.TextField(max_length=500)
+  city = models.ForeignKey(City, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f'{self.title}, {self.city}'
