0687a09a1298bac8a0c781baaed523b53649a8cd
r-mckeith <r.mckeith@me.com>
2021-03-15 15:40:52 -0700

    add profile page route, view and template

--- main_app/views.py
+++ main_app/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
   return render(request, 'home.html')
+
+def profile(request):
+  return render(request, 'profile.html')
