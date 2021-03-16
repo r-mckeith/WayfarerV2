9bdae2b83c90a2fb2a499e5f22965ea5bf25d117
r-mckeith <r.mckeith@me.com>
2021-03-15 18:11:39 -0700

    Create cities show page, added create post functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
   path('', views.home, name='home'),
   path('posts/', views.posts_index, name="posts_index"),
   path('posts/<int:post_id>/', views.post_show, name="post_show"),
+  path('posts/new/', views.post_new, name="post_new"),
   path('cities/', views.cities_index, name='cities_index'),
+  path('cities/<int:city_id>/', views.city_show, name='city_show'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import Post
+from django.shortcuts import render, redirect
+from .models import Post, City
+from .forms import PostForm
 
 # Create your views here.
 def home(request):
@@ -13,5 +14,24 @@ def post_show(request, post_id):
   post = Post.objects.get(id=post_id)
   return render(request, 'posts/show.html', { 'post': post })
 
+def post_new(request):
+  post_form = PostForm(request.POST or None)
+  if request.POST and post_form.is_valid():
+    new_post = post_form.save(commit=False)
+    new_post.user = request.user
+    new_post.city_id = request.POST['cityId']
+    new_post.save()
+    return redirect('posts_index')
+  else:
+    return render(request, 'posts/new.html', { 'post_form': post_form })
+
 def cities_index(request):
   return render(request, 'cities/index.html')
+
+def city_show(request, city_id):
+  post_form = PostForm(request.POST or None)
+  city = City.objects.get(id=city_id)
+  return render(request, 'cities/show.html', { 
+    'city': city,
+    'post_form': post_form
+    })
