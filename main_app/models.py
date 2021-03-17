59853ba2f70dc2375d0506aafc9f1793d600f748
r-mckeith <r.mckeith@me.com>
2021-03-17 11:36:19 -0700

    add iconic photo to city show page

--- main_app/models.py
+++ main_app/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class City(models.Model):
   name = models.CharField(max_length=100, unique=True)
+  photo_url = models.CharField(max_length=500)
 
   def __str__(self):
     return self.name
