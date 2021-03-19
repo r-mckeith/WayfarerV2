990d8ad490f00cd04dc82a04ac601f0cae803596
r-mckeith <r.mckeith@me.com>
2021-03-18 20:36:18 -0700

    play around with comment replies

--- main_app/models.py
+++ main_app/models.py
@@ -28,7 +28,7 @@ class Post(models.Model):
   city = models.ForeignKey(City, on_delete=models.CASCADE)
   user = models.ForeignKey(User, on_delete=models.CASCADE)
 
-  def __str__(self, response):
+  def __str__(self):
     return f'{self.title}, {self.city}'
 
   def posted(self):
