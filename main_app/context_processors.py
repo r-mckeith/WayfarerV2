805a39f4043f2f6f624a39ab10c6895ab8a16eef
r-mckeith <r.mckeith@me.com>
2021-03-21 20:11:54 -0700

    remove testme, add authentication to add city

--- main_app/context_processors.py
+++ main_app/context_processors.py
@@ -1,12 +1,10 @@
-from .models import Post, City
+from .models import City
 
-posts = Post.objects.all().order_by('-created_at')
 cities = City.objects.all()
 
 
 def add_variable_to_context(request):
     return {
         'testme': 'Hello world!',
-        'posts': posts,
         'cities': cities
     }
